3944c77dcbeb7b891c834afd1476bee5cfbdf434
Cluster-Taek <yst811@naver.com>
2020-05-16 19:37:23 +0900

    초기화 시작

---
+++ app/src/main/java/com/cookandroid/payproject/MainActivity.java
@@ -0,0 +1,52 @@
+package com.cookandroid.payproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 전체화면인 DrawerLayout 객체 참조
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Drawer 화면(뷰) 객체 참조
+        final View drawerView = (View) findViewById(R.id.drawer);
+
+
+        // 드로어 화면을 열고 닫을 버튼 객체 참조
+        Button btnOpenDrawer = (Button) findViewById(R.id.btn_OpenDrawer);
+        Button btnCloseDrawer = (Button) findViewById(R.id.btn_CloseDrawer);
+
+
+        // 드로어 여는 버튼 리스너
+        btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+
+        // 드로어 닫는 버튼 리스너
+        btnCloseDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(drawerView);
+            }
+        });
+
+        
+
+
+    }
+}
